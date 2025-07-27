8fd8263b5d6affa805044eef21e414720c75c283
yz13-dev <yztheceo@yandex.ru>
2025-07-27 15:33:19 +0500

    add react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+
+  return {
+    plugins: [
+      tailwindcss(),
+      reactRouter(),
+      tsconfigPaths(),
+      babel({
+        filter: /\.[jt]sx?$/,
+        babelConfig: {
+          presets: ["@babel/preset-typescript"], // if you use TypeScript
+          plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+        },
+      }),
+    ],
+  }
 });
